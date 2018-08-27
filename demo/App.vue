8b7881389b9e350b9e40e7c9d0cfc850015a2a5b
左盐 <huabinglan@163.com>
2018-08-27 19:24:45 +0800

    fixbug

--- demo/App.vue
+++ demo/App.vue
@@ -17,60 +17,52 @@
 }
 </style>
 <template>
-<div id="app" style="width:1000px;margin:100px auto;">
-    <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <h2 class="title">alert</h2>
-  <el-button type="primary" @click="submitHandle">提交</el-button>
-  <h2 class="title">confirm</h2>
-  <el-button type="primary" @click="confirmHandle">删除</el-button>
-  <h2 class="title">msg</h2>
-  <el-button type="primary" @click="msgHandle">msg</el-button>
-  <el-button type="primary" @click="msg1Handle">带回调</el-button>
-  <h2 class="title">loading</h2>
-  <el-button type="primary" @click="loadingHandle">默认样式</el-button>
-  <h2 class="title">tips</h2>
-  <el-button type="primary" id="tips" @click="tipsHandle">上</el-button>
-  <el-button type="primary" id="tips1" @mouseenter.native="tips1Handle">右</el-button>
-  <el-button type="primary" id="tips2" @click="tips2Handle">下</el-button>
-  <el-button type="primary" id="tips3" @click="tips3Handle">左-自定义样式</el-button>
-  <h2 class="title">page</h2>
-  <el-button type="primary" id="tips" @click="pageHandle">自定义</el-button>
-</div>
+  <div id="app" style="width:1000px;margin:100px auto;">
+    <h2 class="title">alert</h2>
+    <el-button type="primary" @click="submitHandle">提交</el-button>
+    <h2 class="title">confirm</h2>
+    <el-button type="primary" @click="confirmHandle">删除</el-button>
+    <h2 class="title">msg</h2>
+    <el-button type="primary" @click="msgHandle">msg</el-button>
+    <el-button type="primary" @click="msg1Handle">带回调</el-button>
+    <h2 class="title">loading</h2>
+    <el-button type="primary" @click="loadingHandle">默认样式</el-button>
+    <h2 class="title">tips</h2>
+    <el-button type="primary" id="tips" @click="tipsHandle">上</el-button>
+    <el-button type="primary" id="tips1" @mouseenter.native="tips1Handle">右</el-button>
+    <el-button type="primary" id="tips2" @click="tips2Handle">下</el-button>
+    <el-button type="primary" id="tips3" @click="tips3Handle">左-自定义样式</el-button>
+    <h2 class="title">page</h2>
+    <el-button type="primary" id="tips" @click="pageHandle">自定义</el-button>
+    <br>
+    <br>
+    <br>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="日期" width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="submitHandle(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="confirmHandle(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -79,7 +71,30 @@ import formComp from "./form.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
   },
   components: {
     "base-form": formComp
